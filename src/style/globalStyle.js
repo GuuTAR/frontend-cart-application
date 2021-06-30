3c705b4929910fc4447ba2e3abc96feee0768af9
GuuTAR <guitar20122012@gmail.com>
2021-06-30 22:53:23 +0700

    Split to layoutStyle and colorStyle

--- src/style/globalStyle.js
+++ src/style/globalStyle.js
@@ -1,11 +1,44 @@
 import { StyleSheet } from "react-native";
-import { black, pink } from "./variables";
+import { black, darkPink, gray, pink, white } from "./variables";
 
-export const globalStyle = StyleSheet.create({
+export const colorStyle = StyleSheet.create({
+    grayBG: {
+        backgroundColor: gray,
+    },
+    whiteBG: {
+        backgroundColor: white,
+    },
+    darkPinkBG: {
+        backgroundColor: darkPink,
+    },
     pinkText: {
         color: pink,
     },
     blackText: {
         color: black,
+    },
+    whiteText: {
+        color: white,
+    }
+})
+
+export const layoutStyle = StyleSheet.create({
+    verMargin10: {
+        marginVertical: 10
+    },
+    horMargin10: {
+        marginHorizontal: 10
+    },
+    row: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    col: {
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+    },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
