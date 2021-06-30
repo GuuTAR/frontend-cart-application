efd31c79762231d963643405dd1927e89d96ec3a
GuuTAR <guitar20122012@gmail.com>
2021-06-30 17:39:00 +0700

    Init global styles

---
+++ src/style/globalStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { black, pink } from "./variables";
+
+export const globalStyle = StyleSheet.create({
+    pinkText: {
+        color: pink,
+    },
+    blackText: {
+        color: black,
+    }
+})
