f7e8755f2591a5da96138d10cea58d57bbc51bdf
GuuTAR <guitar20122012@gmail.com>
2021-07-02 00:34:07 +0700

    Add CartHeader Component for Cart Page

---
+++ src/page/cart/component/header/sheet.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const stylesheet = StyleSheet.create({
+    headerContainer: {
+        alignItems: 'center',
+        width: '100%'
+    },
+    backButtonTitle: {
+        fontSize: 23,
+        paddingHorizontal: 10,
+    },
+    headerTitle: {
+        width: '75%',
+        fontSize: 23,
+    },
+})
