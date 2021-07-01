68e1751ca5672b7940839f36df564717478dba5c
GuuTAR <guitar20122012@gmail.com>
2021-07-01 16:08:30 +0700

    Add productCard Component and styles for homepage

---
+++ src/page/home/component/productCard/sheet.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export const styleSheet = StyleSheet.create({
+    noWrap: {
+        flexWrap: 'nowrap'
+    },
+    productCard: {
+        width: "48%",
+
+        padding: 15,
+        
+        marginHorizontal: 3,
+        marginVertical: 3,
+
+        borderRadius: 10,
+    },
+    productContent: {
+        width: '100%'
+    },
+    productImg: {
+        width: "100%",
+        height: 130
+    },
+    firstProductImg: {
+        width: "50%",
+    },
+    firstProductCard: {
+        width: "97%",
+    },
+    firstProductContent: {
+        width: '50%'
+    },
+    productName: {
+        fontSize: 20
+    },
+    productDes: {
+        width: '100%'
+    },
+    price: {
+        textAlign: 'right'
+    }
+})
