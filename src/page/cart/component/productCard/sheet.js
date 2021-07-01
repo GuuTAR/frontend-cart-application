802466fef1feb325d97f85e4578a5a8a1e996273
GuuTAR <guitar20122012@gmail.com>
2021-07-02 00:34:26 +0700

    Add ProductCard Component for Cart Page

---
+++ src/page/cart/component/productCard/sheet.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export const stylesheet = StyleSheet.create({
+    productCard: {
+        width: '96%',
+
+        marginVertical: 5,
+        padding: 10,
+
+        borderRadius: 15
+    },
+    contentWrapper : {
+        width: '60%',
+        marginLeft: 10,
+
+        justifyContent: 'space-around'
+    },
+    productImg: {
+        width: '35%',
+        height: 120
+    },
+    productName: {
+        fontSize: 18
+    },
+    productDes: {
+        width: '100%'
+    },
+
+    actionWrapper: {
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+})
---
+++ src/page/cart/component/productCard/style.js
@@ -0,0 +1,13 @@
+import { colorStyle, textStyle } from "../../../../style/globalStyle";
+import { stylesheet } from "./sheet";
+
+export const productCard = [stylesheet.productCard, colorStyle.whiteBG]
+
+export const contentWrapper = [stylesheet.contentWrapper]
+
+export const productImg = [stylesheet.productImg]
+export const productName = [stylesheet.productName, textStyle.bold]
+export const productDes = [stylesheet.productDes]
+export const productPrice = [colorStyle.thinPink]
+
+export const actionWrapper = [stylesheet.actionWrapper]
