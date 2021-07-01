405f74e8e5bbe0e4b250bdd5474c370a8eda8101
GuuTAR <guitar20122012@gmail.com>
2021-07-01 16:07:59 +0700

    Add home page and styles

---
+++ src/page/home/sheet.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const stylesheet = StyleSheet.create({
+    productContainer: {
+        width: "100%",
+        justifyContent: 'center'
+    },
+    searchBar: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+        width: "100%"
+    },
+    search: {
+        flex: 1,
+        justifyContent: 'center',
+        width: 100,
+        height: 50,
+        borderRadius: 10,
+
+        paddingVertical: 0,
+        marginRight: 10,
+    },
+    input: {
+        height: 20,
+        margin: 0,
+    }
+})
