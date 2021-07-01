854822908989e25b5357e02ff46bbdb525d077c6
GuuTAR <guitar20122012@gmail.com>
2021-07-02 00:35:20 +0700

    Remove style that unused

--- src/page/home/style.js
+++ src/page/home/style.js
@@ -1,33 +1,7 @@
-import { StyleSheet } from "react-native";
 import { layoutStyle } from "../../style/globalStyle";
 import { stylesheet } from "./sheet";
 
 export const searchBar = [stylesheet.searchBar]
-export const productContainer = [layoutStyle.row, stylesheet.productContainer]
-
-export const style = StyleSheet.create({
-    productContainer: {
-        width: "100%",
-        justifyContent: 'center'
-    },
-    searchBar: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 8,
-        width: "100%"
-    },
-    search: {
-        flex: 1,
-        justifyContent: 'center',
-        width: 100,
-        height: 50,
-        borderRadius: 10,
-
-        paddingVertical: 0,
-        marginRight: 10,
-    },
-    input: {
-        height: 20,
-        margin: 0,
-    }
-})
+export const productContainer = [stylesheet.productContainer]
+export const searchContainer = [stylesheet.searchContainer]
+export const input = [stylesheet.input]
