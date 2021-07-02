69aa1d0bad2dfe7c12f77628eed5c2d21efe86dd
GuuTAR <guitar20122012@gmail.com>
2021-07-02 17:39:14 +0700

    Remove unused import

--- src/page/cart/style.js
+++ src/page/cart/style.js
@@ -1,5 +1,5 @@
 import { stylesheet } from "./sheet";
-import { colorStyle, textStyle } from "../../style/globalStyle";
+import { colorStyle } from "../../style/globalStyle";
 
 export const cartPage = [colorStyle.darkPinkBG, stylesheet.cartPage]
 export const productContainer = [stylesheet.productContainer]
