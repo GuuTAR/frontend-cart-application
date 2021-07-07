3abfd305a46e452ab8bf165ca92c3ce816941484
GuuTAR <guitar20122012@gmail.com>
2021-07-08 02:26:41 +0700

    editItemInCart if count change

--- src/page/cart/component/productCard/ProductCard.js
+++ src/page/cart/component/productCard/ProductCard.js
@@ -1,24 +1,43 @@
 import { Image, Text, View } from "react-native"
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import Row from "../../../../component/Row"
 import productPNG from '../../../../assets/product-example.png'
 import Col from "../../../../component/Col"
-import { actionWrapper, contentWrapper, productCard, productDes, productImg, productName, productPrice } from "./style"
+import { actionWrapper, contentWrapper, productCard, productDes, productImg, productName, productNameWrapper, productPrice } from "./style"
 import InputNumber from "../../../../component/InputNumber/InputNumber"
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { useStore } from '../../../../global_store/useStore'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
+
+    console.log(product)
+
+    const { deleteItemInCart, editItemInCart } = useStore().cartStore
+
+    const [count, setCount] = useState(product?.count || 1)
+
+    const handleDeleteItemFromCart = () => deleteItemInCart(product.id)
+
+    useEffect(() => {
+        editItemInCart({...product, count}, product.id)
+    }, [count])
+    
+    console.log(product.price)
     return (
         <View style={productCard}>
             <Row>
                 <Image source={productPNG} style={productImg} />
                 <Col containerStyle={contentWrapper}>
-                    <Text style={productName}>Product</Text>
+                    <Row containerStyle={productNameWrapper}>
+                        <Text style={productName}>{product?.name || "Product"}</Text>
+                        <FontAwesome5 name="trash-alt" size={20} onPress={handleDeleteItemFromCart} />
+                    </Row>
                     <Row containerStyle={productDes}>
-                        <Text>na dattebayo! dattebayo! dattebayo! dattebayo! dattebayo!</Text>
+                        <Text>{product?.desc || "Description"}</Text>
                     </Row>      
                     <Row containerStyle={actionWrapper}>
-                        <InputNumber min={1} max={100} />
-                        <Text style={productPrice}>150฿</Text>
+                        <InputNumber min={1} max={100} defaultValue={product?.count || 1} setNumber={setCount} />
+                        <Text style={productPrice}>{ product?.price || 150}฿</Text>
                     </Row> 
                 </Col>
             </Row>
