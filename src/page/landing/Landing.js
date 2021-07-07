0c1230e6978a2ff779950f5a9018824c346c91cc
GuuTAR <guitar20122012@gmail.com>
2021-07-07 10:32:21 +0700

    Add nav to Login Logout

--- src/page/landing/Landing.js
+++ src/page/landing/Landing.js
@@ -3,15 +3,19 @@ import React from "react"
 import { background, landingPage, landingTitle, menu, menuContainer, signInButton, signOutButton, signOutText } from "./style"
 import { Button } from "react-native-elements"
 
-const LandingPage = () => {
+const LandingPage = ({navigation}) => {
+
+    const navSignIn = () => navigation.push('SignIn')
+    const navSignOut = () => navigation.push('SignOut')
+
     return (
         <View style={landingPage}>
             <View style={background} />
             <View style={menuContainer}>
                 <View style={menu}>
                     <Text style={landingTitle}>ยินดีต้อนรับสู่แอพ XXXX</Text>
-                    <Button title="Sign In" buttonStyle={signInButton} />
-                    <Button title="Sign Out" buttonStyle={signOutButton} titleStyle={signOutText} />
+                    <Button title="Sign In" buttonStyle={signInButton} onPress={() => navSignIn()} />
+                    <Button title="Sign Up" buttonStyle={signOutButton} titleStyle={signOutText} onPress={navSignOut}  />
                 </View>
             </View>
         </View>
