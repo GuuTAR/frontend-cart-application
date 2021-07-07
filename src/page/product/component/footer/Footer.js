7d979400c2d2bd4cf5f77b6e744362ae8fa9926d
GuuTAR <guitar20122012@gmail.com>
2021-07-08 02:28:19 +0700

    Add handle add item to cart

--- src/page/product/component/footer/Footer.js
+++ src/page/product/component/footer/Footer.js
@@ -1,19 +1,30 @@
 import { Text, View } from "react-native"
-import React from "react"
+import React, { useState } from "react"
 import { addToCartButton, buttonWrapper, footerContainer, inputContainer } from "./style"
 import Row from "../../../../component/Row"
 import InputNumber from '../../../../component/InputNumber/InputNumber'
 import { Button } from "react-native-elements"
+import { useStore } from "../../../../global_store/useStore"
 
-const ProductFooter = () => {
+const ProductFooter = ({ product, hide }) => {
+
+    const { addItemToCart } = useStore().cartStore
+
+    const [count, setCount] = useState(1)
+
+    const handleAddItemToCart = () => {
+        addItemToCart({ ...product, count })
+        hide()
+    }
+    
     return (
         <View style={footerContainer} >
             <Row containerStyle={inputContainer}>
                 <Text>จำนวน</Text>
-                <InputNumber min={1} max={10} />
+                <InputNumber min={1} max={10} setNumber={setCount} />
             </Row>
             <View style={buttonWrapper}>
-                <Button title="เพิ่มลงตะกร้า" buttonStyle={addToCartButton} />
+                <Button title="เพิ่มลงตะกร้า" buttonStyle={addToCartButton} onPress={handleAddItemToCart} />
             </View>
         </View>
     )
