9bfb48dae442b77bb410b10b19bb30a2ba039169
GuuTAR <guitar20122012@gmail.com>
2021-07-07 15:04:25 +0700

    Add register handle

--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -1,10 +1,25 @@
 import { Text, View } from "react-native"
-import React from "react"
+import React, { useState } from "react"
 import { background, formContainer, registerButton, registerContainer, registerPage, registerTitle } from "./style"
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5"
 import { Button, CheckBox, Input } from "react-native-elements"
+import { useStore } from "../../global_store/useStore"
+import { isSomeInvalidValue } from "../../function/global"
 
 const RegisterPage = () => {
+
+    const authStore = useStore().authStore
+
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [rePassword, setRePassword] = useState()
+    const [policyCheck, setPolicyCheck] = useState(false)
+
+    const handleRegister = () => {
+        if (!isSomeInvalidValue(email) && !isSomeInvalidValue(password) && password === rePassword && policyCheck)
+            authStore.register(email, password)
+    }
+
     return (
         <View style={registerPage}>
             <View style={background}>
@@ -16,24 +31,28 @@ const RegisterPage = () => {
                         label="อีเมล"
                         placeholder="email@address.com"
                         leftIcon={<FontAwesome5Icon name="user-alt" />}
+                        onChange={e => setEmail(e.nativeEvent.text)}
                     />
                     <Input
                         label="รหัสผ่าน"
                         placeholder="password"
                         leftIcon={<FontAwesome5Icon name="key" />}
                         secureTextEntry
+                        onChange={e => setPassword(e.nativeEvent.text)}
                     />
                     <Input
                         label="ยืนยันรหัสผ่าน"
                         placeholder="re-password"
                         leftIcon={<FontAwesome5Icon name="key" />}
                         secureTextEntry
+                        onChange={e => setRePassword(e.nativeEvent.text)}
                     />
                     <CheckBox
                         title='ยอมรับเงื่อนไข รายละเอียดเพิ่มเติม'
-                        checked={false}
+                        checked={policyCheck}
+                        onPress={() => setPolicyCheck(!policyCheck)}
                     />
-                    <Button title="ยืนยัน" buttonStyle={registerButton} />
+                    <Button title="ยืนยัน" buttonStyle={registerButton} onPress={handleRegister} />
                 </View>
             </View>
         </View>
