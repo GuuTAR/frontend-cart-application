e6bbf9ddb259cdae13fd8b082b7bec0a4cde0f80
GuuTAR <guitar20122012@gmail.com>
2021-07-07 10:30:24 +0700

    Init StoreProvider at head element

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { StoreProvider } from './src/global_store/storeProvider';
+import React from 'react'
+import RootStore from './src/global_store/store/RootStore';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+    <StoreProvider value={new RootStore()}>
+        <App />
+    </StoreProvider>
+)
+
+AppRegistry.registerComponent(appName, () => Root);
