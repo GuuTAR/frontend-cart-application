dc8e58cbb6ec0d124e8d51dcc8dc038bca985667
GuuTAR <guitar20122012@gmail.com>
2021-07-07 10:29:53 +0700

    Integrate Firebase

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RootRouter from './src/route/RootRouter';
+import auth from '@react-native-firebase/auth'
+import { useStore } from './src/global_store/useStore';
 
 const App = () => {
+
+  const authStore = useStore().authStore
+
+  const [initializing, setInitializing] = useState(true)
+
+  const onAuthStateChanged = (user) => {
+    console.log(user.email, user.displayName)
+    authStore.setUserData(user.email, user.displayName)
+    if (initializing) setInitializing(false)
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
+    return subscriber
+  }, [])
+
+  if (initializing) return null;
   return (
     <NavigationContainer>
       <RootRouter />
