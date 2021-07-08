5fa7256947c75359075067e498ac2e38181743f0
GuuTAR <guitar20122012@gmail.com>
2021-07-08 12:47:52 +0700

    Add readmoreCutter

--- src/function/global.js
+++ src/function/global.js
@@ -2,3 +2,6 @@ export const isnull = (value) => value === null
 export const isUndefined = (value) => value === undefined
 export const isEmptyString = (value) => value === ''
 export const isSomeInvalidValue = (value) => isnull(value) || isUndefined(value) || isEmptyString(value)
+
+export const readmoreCutter = (text, useLength = 95) => 
+    text.length > useLength ? text.slice(0, useLength) + '...อ่านเพิ่มเติม' : text
