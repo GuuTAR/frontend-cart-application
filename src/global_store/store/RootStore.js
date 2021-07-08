138170b1066b2c38177c541dd914e4a868a026cc
GuuTAR <guitar20122012@gmail.com>
2021-07-08 12:18:47 +0700

    Init HomeStore

--- src/global_store/store/RootStore.js
+++ src/global_store/store/RootStore.js
@@ -1,12 +1,15 @@
 import AuthStore from "./AuthStore"
 import CartStore from "./CartStore"
+import HomeStore from "./HomeStore"
 
 export default class RootStore {
   authStore
   cartStore
+  homeStore
 
   constructor() {
       this.authStore = new AuthStore(this)
       this.cartStore = new CartStore(this)
+      this.homeStore = new HomeStore(this)
   }
 }
