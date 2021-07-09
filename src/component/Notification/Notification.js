64277b60433037afe752a2e346dd5fd5b67237c9
GuuTAR <guitar20122012@gmail.com>
2021-07-10 01:33:44 +0700

    Add Notification Component

---
+++ src/component/Notification/Notification.js
@@ -0,0 +1,25 @@
+import { showMessage } from "react-native-flash-message";
+
+const showNoti = (title, desc, type) => {
+    console.log(type)
+    showMessage({
+        message: title,
+        description: desc,
+        type: type,
+        icon: type
+    })
+}
+
+export const notification = (title, desc, type) => {
+    switch (type) {
+        case 'success':
+            showNoti(title, desc, type)
+            return
+        case 'error':
+            showNoti(title, desc, 'danger')
+            return 
+        default:
+            showNoti(title, desc)
+            return
+    }
+}
