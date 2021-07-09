c7cd6a298150dade5c7037d53b041b79f8171a9a
GuuTAR <guitar20122012@gmail.com>
2021-07-10 01:35:11 +0700

    Use notification instead log

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,25 +1,23 @@
 import auth from '@react-native-firebase/auth'
+import { notification } from '../component/Notification/Notification';
 
 export const firebaseLogin = (email, password) => {
     return auth()
         .signInWithEmailAndPassword(email, password)
-        .then(() => {
-            console.log('User account signed in!');
-        })
+        .then(() => notification('Login success!', 'Enjoy your shopping', 'success'))
         .catch(error => {
-            if (error.code === 'auth/email-already-in-use') {
-                console.log('That email address is already in use!');
-            }
-
-            if (error.code === 'auth/invalid-email') {
-                console.log('That email address is invalid!');
-            }
-
-            if (error.code === 'auth/email-already-in-use')
-                console.log('That email address is invalid!');
-
-            
-            console.error(error);
+            console.log(error);
+            switch (error.code) {
+                case 'auth/wrong-password':
+                    notification('Login fail!', 'Email or password is incorrect', 'error')
+                    return;
+                case 'auth/too-many-requests':
+                    notification('Login fail!', 'Spam login please try again later', 'error')
+                    return;
+                default:
+                    notification('Login fail!', 'Please try again later', 'error')
+                    return;
+            } 
         });
 }
 
