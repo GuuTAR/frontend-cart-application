7998fda714e380f6d816f3411ea86fd02012aac9
GuuTAR <guitar20122012@gmail.com>
2021-07-15 15:04:03 +0700

    Change background style

--- src/page/previewImg/style.js
+++ src/page/previewImg/style.js
@@ -1,11 +1,7 @@
 import { colorStyle, layoutStyle } from "../../style/globalStyle"
 import { stylesheet } from "./sheet"
 
-export const previewImgContainer = [stylesheet.previewImgContainer, colorStyle.blackBG, layoutStyle.center]
-
-export const image = [stylesheet.image]
-
-export const buttonWrapper = [stylesheet.buttonWrapper]
+export const buttonWrapper = [stylesheet.buttonWrapper, colorStyle.genBlackBG]
 
 export const backText = [stylesheet.backText, colorStyle.whiteText]
 export const saveButton = [stylesheet.saveButton, colorStyle.darkPinkBG]
