260c3e4e0668c76141e5c50b3a4256ac4d03d9db
GuuTAR <guitar20122012@gmail.com>
2021-07-15 15:03:11 +0700

    Add index state

--- src/page/previewImg/PreviewImg.js
+++ src/page/previewImg/PreviewImg.js
@@ -1,28 +1,33 @@
-import { View, Image, Text } from "react-native"
-import React from "react"
-import { backText, buttonWrapper, image, previewImgContainer, saveButton } from "./style"
+import { Text, Modal } from "react-native"
+import React, { useState } from "react"
+import { backText, buttonWrapper, saveButton } from "./style"
 import Row from "../../component/Row"
 import { Button } from "react-native-elements"
 import { downloadFileToGallary } from "../../function/global"
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 const PreviewImg = ({ navigation, route }) => {
 
+    const [index, setIndex] = useState(0)
+
     const { name, url } = route.params
 
-    const handleDownload = () => downloadFileToGallary(name, url)
+    const handleDownload = () => downloadFileToGallary(`${name}-${index}`, images[index].url)
     const handleGoBack = () => navigation.goBack()
 
+    const images = [
+        { url: url },
+        { url: "https://avatars2.githubusercontent.com/u/7970947?v=3&s=460" }
+    ]
+
     return (
-        <View style={previewImgContainer}>
-            <Image
-                source={{ uri: url }}
-                style={image}
-            />
+        <Modal visible={true} transparent={true}>
+            <ImageViewer imageUrls={images} onChange={(idx) => setIndex(idx)} />
             <Row containerStyle={buttonWrapper}>
                 <Text onPress={handleGoBack} style={backText}>{"< back"}</Text>
                 <Button title="Save" onPress={handleDownload} buttonStyle={saveButton} />
             </Row>
-        </View>
+        </Modal>
     )
 }
 
